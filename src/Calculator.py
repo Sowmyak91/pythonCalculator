b74815834cdca69fa75e33bf551aec955b5ae508
Sowmyak91 <78493957+Sowmyak91@users.noreply.github.com>
2021-03-22 23:42:03 -0400

    Feature: Added Multiply, divide, square and square root methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,13 +1,41 @@
+from CsvReader import CsvReader
+
+
 def addition(a, b):
-    return a + b
+    a = int(a)
+    b = int(b)
+    c = b + a
+    return c
 
 
 def subtraction(a, b):
-    return a - b
+    a = int(a)
+    b = int(b)
+    c = b - a
+    return c
+
+
+def multiplication(a, b):
+    a = int(a)
+    b = int(b)
+    c = b * a
+    return c
+
+
+def division(a, b):
+    a = int(a)
+    b = int(b)
+    c = a / b
+    return c
+
+
+def mean(data):
+    mean = data
+    return mean
 
 
 class Calculator:
-    result = 0
+    result = int()
 
     def __init__(self):
         pass
@@ -19,3 +47,22 @@ class Calculator:
     def subtract(self, a, b):
         self.result = subtraction(a, b)
         return self.result
+
+    def multiply(self, a, b):
+        self.result = multiplication(a, b)
+        return self.result
+
+    def divide(self, a, b):
+        self.result = division(a, b)
+        return self.result
+
+
+class CSVStats(Calculator):
+    data = []
+
+    def __init__(self, data_file):
+        self.data = CsvReader(data_file)
+        pass
+
+    def mean(self):
+        mean(self.data)
